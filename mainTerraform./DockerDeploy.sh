fe69ca29d2e8f16f9a71f512f81449656c5dd52a
Jorge Molina <138607757+Jmo-101@users.noreply.github.com>
2023-11-04 21:21:16 -0400

    Update DockerDeploy.sh

--- mainTerraform./DockerDeploy.sh
+++ mainTerraform./DockerDeploy.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+# Install Docker Engine
+# Add Docker's official GPG key:
+apt-get update
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+# Install Required Packages
+apt-get install -y default-jre 
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get install -y python3.7
+apt-get install -y python3.7-venv 
+apt-get install -y build-essential
+apt-get install -y libmysqlclient-dev 
+apt-get install -y python3.7-dev
